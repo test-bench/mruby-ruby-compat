d7b58c047557c2c11145506178bfab64b2a53e64
Nathan Ladd <nathanladd@gmail.com>
2020-08-08 15:27:37 -0500

    Dir[] is an alias to Dir.glob()

--- src/mrbgem.c
+++ src/mrbgem.c
@@ -9,20 +9,35 @@
 
 #include "debug.h"
 
-static mrb_value mrb_f_exception(mrb_state* mrb, mrb_value self) {
+static mrb_value mrb_f_exception_cause(mrb_state* mrb, mrb_value self) {
   return mrb_nil_value();
 }
 
+static mrb_value mrb_dir_brackets(mrb_state* mrb, mrb_value self) {
+  mrb_int argc;
+  mrb_value* argv;
+  mrb_value block;
+
+  mrb_get_args(mrb, "*!&", &argv, &argc, &block);
+
+  return mrb_funcall_with_block(mrb, self, mrb_intern_lit(mrb, "glob"), argc, argv, block);
+}
+
 void mrb_test_bench_mruby_gem_init(mrb_state* mrb) {
+  struct RClass* dir_class;
+
   mrb_abort_init(mrb);
   mrb_require_init(mrb);
   mrb_toplevel_binding_init(mrb);
 
-  mrb_define_method(mrb, mrb->eException_class, "cause", mrb_f_exception, MRB_ARGS_NONE());
+  mrb_define_method(mrb, mrb->eException_class, "cause", mrb_f_exception_cause, MRB_ARGS_NONE());
 
   mrb_define_alias(mrb, mrb->kernel_module, "public_send", "send");
 
   mrb_define_alias(mrb, mrb->array_class, "*", "join");
+
+  dir_class = mrb_class_get(mrb, "Dir");
+  mrb_define_class_method(mrb, dir_class, "[]", mrb_dir_brackets, MRB_ARGS_REQ(1) | MRB_ARGS_OPT(1) | MRB_ARGS_BLOCK());
 
   return;
 }
