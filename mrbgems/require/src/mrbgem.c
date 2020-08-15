c124f57581bd9f185469dd3deb1a02769d8ae8dd
Nathan Ladd <nathanladd@gmail.com>
2020-08-14 19:31:41 -0500

    Compiled features can be included via -DMRUBY_REQUIRE_COMPILED_FEATURES

--- mrbgems/require/src/mrbgem.c
+++ mrbgems/require/src/mrbgem.c
@@ -7,6 +7,9 @@
 #include <mruby/require/dir.h>
 #include <mruby/require/require.h>
 
+#include <stdlib.h>
+#include <string.h>
+
 static mrb_value
 mrb_f_load(mrb_state* mrb, mrb_value self) {
   mrb_value path;
@@ -66,6 +69,39 @@ mrb_load_error_path(mrb_state* mrb, mrb_value self) {
   return mrb_iv_get(mrb, self, mrb_intern_lit(mrb, "path"));
 }
 
+#ifndef MRUBY_REQUIRE_COMPILED_FEATURES
+#define MRUBY_REQUIRE_COMPILED_FEATURES ""
+#endif /* MRUBY_REQUIRE_COMPILED_FEATURES */
+
+static const char* const mruby_require_compiled_features = MRUBY_REQUIRE_COMPILED_FEATURES;
+
+static void
+register_initial_compiled_features(mrb_state* mrb) {
+  char* initial_compiled_features;
+  char* strtok_r_saveptr;
+  char* compiled_feature;
+  int len;
+
+  len = strlen(mruby_require_compiled_features);
+
+  initial_compiled_features = malloc(len + 1);
+
+  strncpy(initial_compiled_features, mruby_require_compiled_features, len);
+
+  compiled_feature = initial_compiled_features;
+
+  compiled_feature = strtok_r(compiled_feature, ",", &strtok_r_saveptr);
+  while(compiled_feature != NULL) {
+    mrb_register_compiled_feature(mrb, compiled_feature);
+    compiled_feature = strtok_r(NULL, ",", &strtok_r_saveptr);
+  }
+  mrb_register_compiled_feature(mrb, compiled_feature);
+
+  free(initial_compiled_features);
+
+  return;
+}
+
 void
 mrb_mruby_require_gem_init(mrb_state* mrb) {
   mrb_value require_search_paths;
@@ -96,6 +132,8 @@ mrb_mruby_require_gem_init(mrb_state* mrb) {
   mrb_define_method(mrb, load_error, "path", mrb_load_error_path, MRB_ARGS_NONE());
 
   mrb_define_method(mrb, mrb->kernel_module, "__dir__", mrb_f___dir__, MRB_ARGS_NONE());
+
+  register_initial_compiled_features(mrb);
 
   return;
 }
